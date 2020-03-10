78823b92b98013313f8a75f8864ecef46c4dedc4
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-03-11 00:14:00 +0100

    create set.add()

---
+++ python3/set.add().py
@@ -0,0 +1,9 @@
+########################### my code #######################
+n = int(input())
+my_set = set()
+
+for i in range(n):
+    my_set.add(input())
+
+print(len(my_set))
+######################## end of my code ####################
