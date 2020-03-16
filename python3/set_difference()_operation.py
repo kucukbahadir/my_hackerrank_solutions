48706e31cba2b1efee4d0051f1639f8da9bc394f
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-03-16 13:29:11 +0100

    Create set_difference()_operation.py

---
+++ python3/set_difference()_operation.py
@@ -0,0 +1,8 @@
+############################ my code ############################
+n_english = int(input())
+studentsID_english = input().split()
+n_english = int(input())
+studentsID_french = input().split()
+
+print(len(set(studentsID_english).difference(set(studentsID_french))))
+######################### end of my code ########################
