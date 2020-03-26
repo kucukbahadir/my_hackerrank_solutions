f6bd182c49e993a3aedeb20f28b62657aab294ea
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-03-26 21:48:28 +0100

    Create hurdle-race.py

---
+++ algorithms/hurdle-race.py
@@ -0,0 +1,7 @@
+####################### my code #####################
+def hurdleRace(k, height):
+    if max(height) - k > 0:
+        return max(height) - k
+    else:
+        return 0
+################### end of my code ##################
