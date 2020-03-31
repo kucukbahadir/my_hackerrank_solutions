ae52e47dea6a44c774c551a9dcfb51fea20cfd27
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-03-31 11:01:38 +0200

    Create basicDataTypes.cpp

---
+++ c plus plus/basicDataTypes.cpp
@@ -0,0 +1,19 @@
+//////////////////////// my code ///////////////////////
+int main() {
+    int integerNumber;
+    long longNumber;
+    char character;
+    float floatNumber;
+    double doubleNumber;
+
+    cin >> integerNumber;
+    cin >> longNumber;
+    cin >> character;
+    cin >> floatNumber;
+    cin >> doubleNumber;
+
+    printf("%d\n%ld\n%c\n%3f\n%lf", integerNumber,longNumber, character , floatNumber , doubleNumber);
+
+    return 0;
+}
+/////////////////////  end of my code ////////////////////
