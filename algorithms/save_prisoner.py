526b7e2d030bde631b37f019e23712a63c7de263
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-04-06 23:17:07 +0200

    Create save_prisoner.py

---
+++ algorithms/save_prisoner.py
@@ -0,0 +1,4 @@
+########################## my code #######################
+def saveThePrisoner(n, m, s):
+    return n if (m + s - 1) % n == 0 else (m + s - 1) % n
+####################### end of my code ###################
