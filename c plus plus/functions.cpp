e42f51ace4ebe26654fda7d4ded0afd49d492491
Muhammed Bahadir Kucuk <munisebahadir@gmail.com>
2020-04-25 01:04:02 +0200

    Create functions.cpp

---
+++ c plus plus/functions.cpp
@@ -0,0 +1,11 @@
+////////////////////////////// my code ///////////////////////////
+int max_of_four(int a, int b, int c, int d) {
+    int maxNumber = a;
+
+    if (b > maxNumber) maxNumber = b;
+    if (c > maxNumber) maxNumber = c;
+    if (d > maxNumber) maxNumber = d;
+    
+    return maxNumber;
+}
+/////////////////////////// end of my code ///////////////////////
